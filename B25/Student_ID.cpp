a4fa74cb05d16ae4e803af9ff2c3c563512e82f9
Kamrul Hasan <kamrulkoche@gmail.com>
2023-06-23 04:31:39 +0600

    Third commit

---
+++ B25/Student_ID.cpp
@@ -0,0 +1,251 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter your ID Number:";
+    cin>>n;
+    if(n==20422471)
+    {
+        cout<<"Student Name: SALMAN RAHMAN SHAHED"<<endl;
+        cout<<"Department:CSE"<<endl;
+        cout<<"Section:B25"<<endl;
+    }
+
+    if(n==20429511)
+    {
+        cout<<"Student Name: MAHAMUDUL HASAN PARASH"<<endl;
+        cout<<"Department:CSE"<<endl;
+        cout<<"Section:B25"<<endl;
+    }
+
+    if(n==20431271)
+    {
+        cout<<"Student Name: IMTIAZ AHMED"<<endl;
+        cout<<"Department:CSE"<<endl;
+        cout<<"Section:B25"<<endl;
+    }
+
+    if(n==20433321)
+    {
+        cout<<"Student Name: fARDIN AHAMED OHE"<<endl;
+        cout<<"Department:CSE"<<endl;
+        cout<<"Section:B25"<<endl;
+    }
+    if(n==20433341)
+    {
+        cout<<"Student Name:MD.TANSIR AHMED"<<endl;
+        cout<<"Department:CSE"<<endl;
+        cout<<"Section:B25"<<endl;
+    }
+    if(n==20433361)
+    {
+        cout<<"Student Name: P.M.BIZEN ISLAM BINDU"<<endl;
+        cout<<"Department:CSE"<<endl;
+        cout<<"Section:B25"<<endl;
+    }
+    if(n==20433371)
+    {
+        cout<<"Student Name: PARTHIB BANIK"<<endl;
+        cout<<"Department:CSE"<<endl;
+        cout<<"Section:B25"<<endl;
+    }
+    if(n==20433381)
+    {
+        cout<<"Student Name: TAIFUR MOHAMMAD TANIM"<<endl;
+        cout<<"Department:CSE"<<endl;
+        cout<<"Section:B25"<<endl;
+    }
+    if(n==20433401)
+    {
+        cout<<"Student Name: FAHIM SHAHRIAR SOURAV"<<endl;
+        cout<<"Department:CSE"<<endl;
+        cout<<"Section:B25"<<endl;
+    }
+    if(n==20433441)
+    {
+        cout<<"Student Name: MD.TASNIMUL ALAM PRANTO"<<endl;
+        cout<<"Department:CSE"<<endl;
+        cout<<"Section:B25"<<endl;
+    }
+    if(n==20433451)
+    {
+        cout<<"Student Name: MD.AKRAMUL ISLAM"<<endl;
+        cout<<"Department:CSE"<<endl;
+        cout<<"Section:B25"<<endl;
+    }
+    if(n==20433471)
+    {
+        cout<<"Student Name: DURJOY BARDHAN"<<endl;
+        cout<<"Department:CSE"<<endl;
+        cout<<"Section:B25"<<endl;
+    }
+    if(n==20433481)
+    {
+        cout<<"Student Name: M.ABDULLAH FAIAZ"<<endl;
+        cout<<"Department:CSE"<<endl;
+        cout<<"Section:B25"<<endl;
+    }
+    if(n==20433511)
+    {
+        cout<<"Student Name: M.IMRAN HOSSAIN"<<endl;
+        cout<<"Department:CSE"<<endl;
+        cout<<"Section:B25"<<endl;
+    }
+    if(n==20433521)
+    {
+        cout<<"Student Name: RIFAT TASNIA ISLAM"<<endl;
+        cout<<"Department:CSE"<<endl;
+        cout<<"Section:B25"<<endl;
+    }
+    if(n==20433571)
+    {
+        cout<<"Student Name: MD.PEYAL SARKER"<<endl;
+        cout<<"Department:CSE"<<endl;
+        cout<<"Section:B25"<<endl;
+    }
+    if(n==20433581)
+    {
+        cout<<"Student Name: SATIRTHA MOHANTA BIBYO"<<endl;
+        cout<<"Department:CSE"<<endl;
+        cout<<"Section:B25"<<endl;
+    }
+    if(n==20433591)
+    {
+        cout<<"Student Name: MAISHA SHAMS"<<endl;
+        cout<<"Department:CSE"<<endl;
+        cout<<"Section:B25"<<endl;
+    }
+    if(n==20433601)
+    {
+        cout<<"Student Name: RAFI REZA HUQ"<<endl;
+        cout<<"Department:CSE"<<endl;
+        cout<<"Section:B25"<<endl;
+    }
+    if(n==20433651)
+    {
+        cout<<"Student Name: ZARIN TASNIM"<<endl;
+        cout<<"Department:CSE"<<endl;
+        cout<<"Section:B25"<<endl;
+    }
+    if(n==20433711)
+    {
+        cout<<"Student Name: MARUYA RAZZAK RIYA"<<endl;
+        cout<<"Department:CSE"<<endl;
+        cout<<"Section:B25"<<endl;
+    }
+    if(n==20433721)
+    {
+        cout<<"Student Name: MD.ASHRAFUL HOSSAIN ALIF "<<endl;
+        cout<<"Department:CSE"<<endl;
+        cout<<"Section:B25"<<endl;
+    }
+    if(n==20433731)
+    {
+        cout<<"Student Name: REYAD HOSSAIN"<<endl;
+        cout<<"Department:CSE"<<endl;
+        cout<<"Section:B25"<<endl;
+    }
+    if(n==20433741)
+    {
+        cout<<"Student Name: MD.MAHBUBUR RAHMAN"<<endl;
+        cout<<"Department:CSE"<<endl;
+        cout<<"Section:B25"<<endl;
+    }
+    if(n==20433781)
+    {
+        cout<<"Student Name: ASWAD MONSUR"<<endl;
+        cout<<"Department:CSE"<<endl;
+        cout<<"Section:B25"<<endl;
+    }
+    if(n==20433791)
+    {
+        cout<<"Student Name: HUMANA KHAN"<<endl;
+        cout<<"Department:CSE"<<endl;
+        cout<<"Section:B25"<<endl;
+    }
+    if(n==20433811)
+    {
+        cout<<"Student Name: RASHAT RIZWAN RAYHAN"<<endl;
+        cout<<"Department:CSE"<<endl;
+        cout<<"Section:B25"<<endl;
+    }
+    if(n==20433831)
+    {
+        cout<<"Student Name: BADHON KUMAR SARKER"<<endl;
+        cout<<"Department:CSE"<<endl;
+        cout<<"Section:B25"<<endl;
+    }
+    if(n==20433851)
+    {
+        cout<<"Student Name: FATEMA NUR RAIDAH"<<endl;
+        cout<<"Department:CSE"<<endl;
+        cout<<"Section:B25"<<endl;
+    }
+    if(n==20433861)
+    {
+        cout<<"Student Name:KHANDAKER FATEMA NUR SOHADA"<<endl;
+        cout<<"Department:CSE"<<endl;
+        cout<<"Section:B25"<<endl;
+    }
+    if(n==20433901)
+    {
+        cout<<"Student Name: MD.SAKIB HASAN"<<endl;
+        cout<<"Department:CSE"<<endl;
+        cout<<"Section:B25"<<endl;
+    }
+    if(n==20433911)
+    {
+        cout<<"Student Name: ABU SAKIB MOLLA"<<endl;
+        cout<<"Department:CSE"<<endl;
+        cout<<"Section:B25"<<endl;
+    }
+    if(n==20433941)
+    {
+        cout<<"Student Name: MD EASIN ARAFAT"<<endl;
+        cout<<"Department:CSE"<<endl;
+        cout<<"Section:B25"<<endl;
+    }
+    if(n==20433951)
+    {
+        cout<<"Student Name: SADIA ISLAM"<<endl;
+        cout<<"Department:CSE"<<endl;
+        cout<<"Section:B25"<<endl;
+    }
+    if(n==20433981)
+    {
+        cout<<"Student Name: TASNIA AMIN NEHA "<<endl;
+        cout<<"Department:CSE"<<endl;
+        cout<<"Section:B25"<<endl;
+    }
+    if(n==204334001)
+    {
+        cout<<"Student Name: S.M.NOWSHAN AL-NAHIN PAVEL"<<endl;
+        cout<<"Department:CSE"<<endl;
+        cout<<"Section:B25"<<endl;
+    }
+    if(n==20433991)
+    {
+        cout<<"Student Name: JAKIA NOOR "<<endl;
+        cout<<"Department:CSE"<<endl;
+        cout<<"Section:B25"<<endl;
+    }
+    if(n==20434011)
+    {
+        cout<<"Student Name: RAHAT RAHMAN"<<endl;
+        cout<<"Department:CSE"<<endl;
+        cout<<"Section:B25"<<endl;
+    }
+    if(n==20434051)
+    {
+        cout<<"Student Name: S.M.KAMRUL HASAN KOCHE "<<endl;
+        cout<<"Department:CSE"<<endl;
+        cout<<"Section:B25"<<endl;
+    }
+    else
+    {
+        cout<<"Wrong User ID";
+    }
+    getch ();
+}
